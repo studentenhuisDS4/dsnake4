70345f1f03be20b43442ed4ce81dc7c02252b0d1
David Zwart <davidzwa@gmail.com>
2020-04-16 21:46:35 +0200

    Remove unnecessary things

--- dsnake4-phaser/src/components/Game/Scene.ts
+++ dsnake4-phaser/src/components/Game/Scene.ts
@@ -6,7 +6,6 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 
 export class GameScene extends Phaser.Scene {
     private square!: Phaser.GameObjects.Rectangle & { body: Phaser.Physics.Arcade.Body };
-    private square2!: Phaser.GameObjects.Rectangle & { body: Phaser.Physics.Arcade.Body };
 
     constructor() {
         super(sceneConfig);
@@ -14,15 +13,12 @@ export class GameScene extends Phaser.Scene {
 
     public create() {
         this.square = this.add.rectangle(400, 400, 100, 100, 0xDDDDDD) as any;
-        // this.square2 = this.add.rectangle(400, 600, 100, 100, 0xDDDDDD) as any;
         this.physics.add.existing(this.square);
         console.log("SNAKE SCENE - started");
     }
 
     public update() {
-        // TODO
-        setInterval(() => {
-            console.log("SNAKE SCENE - started");
-        }, 300);
+        // Called every update
+        // TODO fill with game updates
     }
 }

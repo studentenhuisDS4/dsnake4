65a9e9adbe6570e30c0ce41c98e083ff8965ed54
David Zwart <davidzwa@gmail.com>
2020-04-16 17:47:50 +0200

    Added app and a place to start game

---
+++ dsnake4-phaser/src/config/GameConfig.ts
@@ -0,0 +1,24 @@
+import * as Phaser from 'phaser';
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+  title: 'Sample',
+ 
+  type: Phaser.AUTO,
+ 
+  scale: {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+ 
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true,
+    },
+  },
+ 
+  parent: 'game',
+  backgroundColor: '#000000',
+};
+ 
+export const snakeGame = new Phaser.Game(gameConfig);
---
+++ dsnake4-phaser/src/views/Snake.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="snake">
+    <h1>This is an snake game.</h1>
+  </div>
+</template>

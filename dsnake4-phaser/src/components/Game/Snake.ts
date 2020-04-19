662ee2f53ffc1610ea1912f0ef6268df63df2670
David Zwart <davidzwa@gmail.com>
2020-04-20 00:00:11 +0200

    Update to moving snake with keyboard input

--- dsnake4-phaser/src/components/Game/Snake.ts
+++ dsnake4-phaser/src/components/Game/Snake.ts
@@ -2,64 +2,152 @@ type SnakeDirection = 'Up' | 'Right' | 'Left' | 'Down';
 type BodyPartType = 'Head' | 'Body' | 'Tail';
 
 // TODO generalize later
-let cellsX: number = 105;
+let cellsX: number = 80;
 let cellsY: number = 60;
 
-class BodyPart {
+export class BodyPart {
     public x: number;
     public y: number;
     public type: BodyPartType;
+    public gameObject!: Phaser.GameObjects.Text;
+
+    public floor!: number;
 
     constructor(x: number, y: number, type: BodyPartType) {
         this.x = x;
         this.y = y;
         this.type = type;
     }
+
+    public toCharacter() {
+        switch (this.type) {
+            case 'Body':
+                return 'S';
+            case 'Head':
+                return 'D';
+            case 'Tail':
+                return '4';
+            default:
+                return '';
+        }
+    }
 }
 
 export class Snake {
+    x: number;
+    y: number;
     direction: SnakeDirection;
     bodyParts!: BodyPart[];
 
     constructor(initialLength: number, x: number, y: number, initialDirection: SnakeDirection = 'Up') {
         this.direction = initialDirection;
+        this.x = x;
+        this.y = y;
 
-        this.generateSnake(initialLength, x, y);
+        this.emptySnake();
+        this.generateSnake(initialLength);
     }
 
-    private generateSnake(length: number, x: number, y: number) {
-        let xPart = x;
-        let yPart = y;
+    private emptySnake() {
         this.bodyParts = [];
+    }
+
+    private generateSnake(length: number) {
+        let xPart = this.x;
+        let yPart = this.y;
         this.bodyParts.push(new BodyPart(xPart, yPart, 'Head'));
-        console.log("Rendering snake");
+        // console.log("Rendering snake");
         for (let i = 0; i < length - 1; i++) {
             let resultX;
             let resultY;
             switch (this.direction) {
                 case 'Up':
-                    resultY = yPart < cellsY ? yPart-- : null;
-                    break;
-                case 'Down':
                     resultY = yPart > 0 ? yPart-- : null;
                     break;
+                case 'Down':
+                    resultY = yPart < cellsY ? yPart-- : null;
+                    break;
                 case 'Right':
-                    resultX = xPart < cellsX ? xPart++ : null;
+                    resultX = xPart < cellsX ? xPart-- : null;
                     break;
                 case 'Left':
-                    resultX = xPart < cellsX ? xPart-- : null;
+                    resultX = xPart < cellsX ? xPart++ : null;
                     break;
             }
             // Dont allow overflow of body part
             if (resultX != null || resultY != null) {
-                this.bodyParts.push(new BodyPart(xPart, yPart, 'Body'));
+                if (i == length - 2) {
+                    this.bodyParts.push(new BodyPart(xPart, yPart, 'Tail'));
+                } else {
+                    this.bodyParts.push(new BodyPart(xPart, yPart, 'Body'));
+                }
             }
             else {
                 throw new Error("Trying to render Snake BodyPart outside game area.");
             }
         }
+    }
 
-        // We draw Tail with separate character
-        this.bodyParts.push(new BodyPart(xPart, yPart, 'Tail'));
+    public rotateLeft() {
+        switch (this.direction) {
+            case 'Up':
+                this.direction = 'Left';
+                break;
+            case 'Down':
+                this.direction = 'Right';
+                break;
+            case 'Right':
+                this.direction = 'Up';
+                break;
+            case 'Left':
+                this.direction = 'Down';
+                break;
+        }
+    }
+
+    public rotateRight() {
+        switch (this.direction) {
+            case 'Up':
+                this.direction = 'Right';
+                break;
+            case 'Down':
+                this.direction = 'Left';
+                break;
+            case 'Right':
+                this.direction = 'Down';
+                break;
+            case 'Left':
+                this.direction = 'Up';
+                break;
+        }
+    }
+
+    public moveSnake() {
+        switch (this.direction) {
+            case 'Up':
+                this.y < cellsY ? this.y-- : null;
+                break;
+            case 'Down':
+                this.y > 0 ? this.y++ : null;
+                break;
+            case 'Right':
+                this.x < cellsX ? this.x++ : null;
+                break;
+            case 'Left':
+                this.x < cellsX ? this.x-- : null;
+                break;
+        }
+        let newX = this.x;
+        let newY = this.y;
+        this.bodyParts.forEach(part => {
+            const currX = part.x;
+            const currY = part.y;
+            part.x = newX;
+            part.y = newY;
+
+            newX = currX;
+            newY = currY;
+        });
+
     }
 }
--- dsnake4-phaser/src/components/Game/SnakeMainScene.ts
+++ dsnake4-phaser/src/components/Game/SnakeMainScene.ts
@@ -1,12 +1,14 @@
 import * as Phaser from 'phaser';
 import { scaleFactor as SF, SW, SH } from './GameConfig';
-import { Snake } from './Snake';
+import { Snake, BodyPart } from './Snake';
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
     active: false,
     visible: false,
     key: 'Game',
 };
+
+export const SnakeDelayMs: number = 150;
 
 export class SnakeMainScene extends Phaser.Scene {
     // private square!: Phaser.GameObjects.Rectangle & { body: Phaser.Physics.Arcade.Body };
@@ -15,12 +17,21 @@ export class SnakeMainScene extends Phaser.Scene {
 
     private width: number;
     private height: number;
-    private cellsX: number = 105;
+    private cellsX: number = 80;
     private cellsY: number = 60;
     private cellWidth!: number;
     private cellHeight!: number;
 
     private snake!: Snake;
+    private defaultTextStyle = {
+        fontSize: this.cellHeight?.toString(),
+        fontStyle: 'normal',
+        fontFamily: 'Consolas',
+        color: "#42b983",
+        backgroundColor: 'rgba(0,0,0,0)'
+    };
+
+    private timedEvent!: Phaser.Time.TimerEvent;
 
     constructor() {
         super(sceneConfig);
@@ -29,42 +40,63 @@ export class SnakeMainScene extends Phaser.Scene {
     }
 
     public create() {
+        console.log("SNAKE SCENE - created");
+
         this.cellWidth = this.width / this.cellsX;
         this.cellHeight = this.height / this.cellsY;
-        
+
         this.add.grid(
             this.width / 2, this.height / 2,
             this.width + 1, this.height + 1,
             this.cellWidth, this.cellHeight,
             0x000000, 1, 0x222222, 0.9);
 
-        // let defaultTextStyle = {
-        //     fontSize: this.cellHeight,
-        //     fontStyle: 'normal',
-        //     fontFamily: 'Consolas',
-        //     color: "#42b983"
-        // };
+        this.snake = new Snake(15, 16, 3, 'Right');
+        this.renderSnake();
 
-        this.snake = new Snake(3, 3, 3, 'Up');
+        this.input.keyboard.on('keydown-' + 'LEFT', (event: any) => {
+            event.preventDefault();
+            this.snake.rotateLeft();
+        });
+        this.input.keyboard.on('keydown-' + 'A', (event: any) => {
+            event.preventDefault();
+            this.snake.rotateLeft();
+        });
+        this.input.keyboard.on('keydown-' + 'RIGHT', (event: any) => {
+            event.preventDefault();
+            this.snake.rotateRight();
+        });
+        this.input.keyboard.on('keydown-' + 'D', (event: any) => {
+            event.preventDefault();
+            this.snake.rotateRight();
+        });
 
-        // let delta = this.cellWidth;
-        // let i = 1;
-        // for (let j = 0; j < 12; j++) {
-        //     this.add.text(delta * i++ + 3, (j + 3) * delta, "D", defaultTextStyle);
-        //     this.add.text(delta * i++ + 3, (j + 3) * delta, "S", defaultTextStyle);
-        //     this.add.text(delta * i++ + 3, (j + 3) * delta, "S", defaultTextStyle);
-        //     this.add.text(delta * i++ + 3, (j + 3) * delta, "S", defaultTextStyle);
-        //     this.add.text(delta * i++ + 3, (j + 3) * delta, "S", defaultTextStyle);
-        //     this.add.text(delta * i++ + 3, (j + 3) * delta, "S", defaultTextStyle);
-        //     this.add.text(delta * i++ + 3, (j + 3) * delta, "S", defaultTextStyle);
-        //     this.add.text(delta * i++ + 3, (j + 3) * delta, "4", defaultTextStyle);
-        // }
-
-        console.log("SNAKE SCENE - started");
+        this.timedEvent = this.time.addEvent({ delay: SnakeDelayMs, callback: this.onTimedUpdate, callbackScope: this, loop: true });
     }
 
-    public update() {
-        // Called every update
-        // TODO fill with game updates
+    private onTimedUpdate() {
+        this.snake.moveSnake();
+        this.renderSnake();
+    }
+
+    private renderSnake() {
+        if (this.snake?.bodyParts != null) {
+            this.snake.bodyParts.forEach(part => {
+                this.renderSnakePart(part);
+            });
+        }
+    }
+
+    private renderSnakePart(part: BodyPart) {
+        this.snake.bodyParts.forEach(part => {
+            const pixelX = (part.x - 1) * this.cellWidth;
+            const pixelY = (part.y - 1) * this.cellHeight;
+            if (part.gameObject == null) {
+                part.gameObject = this.add.text(pixelX, pixelY, part.toCharacter(), this.defaultTextStyle);
+            }
+            else {
+                part.gameObject.setPosition(pixelX, pixelY);
+            }
+        });
     }
 }
